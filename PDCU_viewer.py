72804d3fa20d639b75494d4d989da8fb4f2b9b90
chroming <guangzhishang@gmail.com>
2015-12-21 21:56:35 +0800

    增加statistic项
    增加三个statistic工作表,由于工作表很长获取耗时较久.

--- PDCU_viewer.py
+++ PDCU_viewer.py
@@ -11,6 +11,9 @@ log = open('/tmp/test.txt','r')
 logfile = log.read()
 log.close()
 
+pdc = xlwt.Workbook()
+plsi=rlsi=vlsi = 6
+
 
 class Logpost(object):
 
@@ -27,7 +30,6 @@ class Logpost(object):
         self.VLS_cut = re.findall(r'Volume Level Statistics(.*?)\=\=\=\=\=',self.logfile,re.S)
 
     def creatxls(self):
-        pdc = xlwt.Workbook()
 
         plctable = pdc.add_sheet('Port Configuration')
         plcsingle = re.findall(r'0x\w.*?U\S*',self.ConData)
@@ -38,35 +40,73 @@ class Logpost(object):
             for plcj in range(0,plclen):
                 plctable.write(plci,plcj,(plc[plcj]))
             plci = plci + 1
-        pdc.save('/tmp/log1.xls')
-        print('11111')
+        pdc.save('/tmp/log.xls')
+
 
 
         rlctable = pdc.add_sheet('Rank Configuration')
-        rlcsingle = re.findall(r'0x\w{1,4}\,0x\w\S*',self.ConData)
-        rlci,rlcj = 5,0
+        rlcsingle = re.findall(r'\s0x\w{1,4}\,0x\w\S*',self.ConData)
+        rlci, rlcj = 5, 0
         for rlcstr in rlcsingle:
             rlc = rlcstr.split(",")
             rlclen = len(rlc)
             for rlcj in range(0,rlclen):
-                print(rlc[rlcj])
+
                 rlctable.write(rlci,rlcj,(rlc[rlcj]))
             rlci = rlci + 1
-        pdc.save('/tmp/log1.xls')
-        print('22222')
+        pdc.save('/tmp/log.xls')
+
 
 
         vlctable = pdc.add_sheet('Volume Configuration')
-        vlcsingle = re.findall(r'0x\w.*?IBM\S*',self.ConData)
+        vlcsingle = re.findall(r'0x\w.*?\,IBM\S*',self.ConData)
         vlci,vlcj = 5,0
         for vlcstr in vlcsingle:
-            vlc = plcstr.split(",")
+            vlc = vlcstr.split(",")
             vlclen = len(vlc)
             for vlcj in range(0,vlclen):
                 vlctable.write(vlci,vlcj,(vlc[vlcj]))
             vlci = vlci + 1
-        pdc.save('/tmp/log1.xls')
-        print('33333')
+        pdc.save('/tmp/log.xls')
+
+    def get_time(self,data_cut):
+        #pds = xlwt.Workbook()
+
+        plsstart = re.findall(r'Interval Start\:\s*(\S*\s*\S*',self.data_cut)[0]
+        plsend = re.findall(r'Interval End\:\s*(\S*\s*\S*',self.data_cut)[0]
+        plength = re.findall(r'Interval Length:\s*(\d*)',self.data_cut)[0]
+
+    def get_pls(self,PLScut,plsi):
+
+        #plstable = pdc.add_sheet('Port Statistics')
+        singlepls = re.findall(r'(0x.*\d)',PLScut)
+
+        for splstr in singlepls:
+            plsj = 6
+            spls = splstr.split(",")
+            for pi in range(0,len(spls)):
+                plstable.write(plsi,plsj+pi,spls[pi])
+            plsi = plsi+1
+
+        pdc.save('/tmp/log.xls')
+        return plsi
+
+
+    def get_rls(self,RLScut,rlsi):
+        #rlstable = pdc.add_sheet('Rank Statistics')
+        singlerls = re.findall(r'(0x.*\d)',RLScut)
+
+        for srplstr in singlerls:
+            rlsj = 6
+            srls = srplstr.split(",")
+            for ri in range(0,len(srls)):
+                rlstable.write(rlsi,rlsj+rj,srls[ri])
+            rlsi = rlsi+1
+        pdc.save('/tmp/log.xls')
+        return  rlsi
+
+
+
 
 
 
@@ -87,6 +127,14 @@ logdata.get_PDC()
 print logdata.PDC_cut
 logdata.get_statistics()
 logdata.creatxls()
-for PLS in logdata.PLS_cut:
-    #print PLS
-    pass
+
+plstable = pdc.add_sheet('Port Statistics')
+for PLSCUT in logdata.PLS_cut:
+
+    plsi = logdata.get_pls(PLSCUT,plsi)
+
+rlstable = pdc.add_sheet('Rank Statistics')
+for RLSCUT in logdata.RLS_cut:
+    rlsi = logdata.get_rls(RLSCUT,rlsi)
+
+
